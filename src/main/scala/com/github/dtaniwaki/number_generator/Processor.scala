6f9635a708cf049f0334fe3e21d8746336b2b628
dtaniwaki <daisuketaniwaki@gmail.com>
2016-04-17 22:54:58 -0700

    Show warning on number generation error

--- src/main/scala/com/github/dtaniwaki/number_generator/Processor.scala
+++ src/main/scala/com/github/dtaniwaki/number_generator/Processor.scala
@@ -2,15 +2,19 @@ package com.github.dtaniwaki.number_generator
 
 import java.time.ZonedDateTime
 
+import com.typesafe.scalalogging.StrictLogging
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-class Processor(numbers: List[Num.Base], observers: List[Observer.Base]) {
+class Processor(numbers: List[Num.Base], observers: List[Observer.Base]) extends StrictLogging {
   def process(): Unit = {
     val t = ZonedDateTime.now.toEpochSecond
     Future.sequence(numbers.map { n =>
       n.generate().recover {
-        case e: Throwable => Map[String, Int]()
+        case e: Throwable =>
+          logger.warn(e.getMessage())
+          Map[String, Int]()
       }
     }).map { counts =>
       val mergedCounts = counts.map(_.keys).flatten.distinct.map { key =>
